675b93a9ca385cc9f46f5dbc1a545ca7b267969b
Henrik Wenz <henrik.wenz@gmail.com>
2018-03-16 21:44:41 +0100

    Improve vendor error

--- src/utils/validate-vendor.js
+++ src/utils/validate-vendor.js
@@ -2,7 +2,8 @@ const vendors = require('../vendors.json')
 
 function validateVendor (vendor) {
   if (!vendors.includes(vendor)) {
-    throw new Error(`Invalid vendor (Allowed: ${vendors.join(',')})`)
+    const error = new Error(`Invalid vendor (Allowed: ${vendors.join(',')})`)
+    throw error
   }
 }
 
