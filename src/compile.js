7071c4041460a85c6098acd73a7e7ba5815ecd5e
N U R O ™ <me@nuro.sh>
2019-06-20 16:02:14 +0100

    Config option passed from CLI to get-config-file util

--- src/compile.js
+++ src/compile.js
@@ -3,8 +3,10 @@ const configureWebpack = require('./webpack-config.js')
 const getConfigFile = require('./utils/get-config-file-config')
 
 module.exports = (env = {}) => {
+  console.log(`\n\nENV: ${JSON.stringify(env)}\n\n`)
+
   // Get user config file
-  const { webpack, ...config } = getConfigFile()
+  const { webpack, ...config } = getConfigFile(env.config)
 
   // Configure userWebpackHook
   const userWebpackHook = webpack || (config => config)
