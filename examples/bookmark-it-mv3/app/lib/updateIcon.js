fa7023294014e4311bf8f048dabb6866e06346ea
here_nerd <here@nerdnative.com>
2022-02-25 22:16:21 +0100

    Add note for KNOWN MV3 ISSUE: chrome.i18n.getMessage not a function

--- examples/bookmark-it-mv3/app/lib/updateIcon.js
+++ examples/bookmark-it-mv3/app/lib/updateIcon.js
@@ -18,6 +18,8 @@ export default async function updateIcon (tabId, hasBookmark) {
     }),
     chrome.action.setTitle({
       tabId,
+      // KNOWN MV3 ISSUE: chrome.i18n.getMessage not a function
+      // See: https://groups.google.com/a/chromium.org/g/chromium-extensions/c/dG6JeZGkN5w
       title: hasBookmark ? chrome.i18n.getMessage('unbookmarkTitle') : chrome.i18n.getMessage('bookmarkTitle')
     })
   ])
