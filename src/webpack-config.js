9375ef9d33c7380b875505f99d06722387d7675d
here_nerd <here@nerdnative.com>
2022-02-25 21:49:08 +0100

    Remove the block of polyfill as I proposed (https://github.com/webextension-toolbox/webextension-toolbox/issues/196#issuecomment-897729508). This polyfill breaks in MV3 and it is no longer needed anyway.

--- src/webpack-config.js
+++ src/webpack-config.js
@@ -118,27 +118,6 @@ module.exports = function webpackConfig ({
   // Add Wilcard Entry Plugin
   config.plugins.push(new GlobEntriesPlugin())
 
-  // Add webextension polyfill
-  if (['chrome', 'opera', 'edge'].includes(vendor)) {
-    config.plugins.push(
-      new webpack.ProvidePlugin({
-        browser: require.resolve('webextension-polyfill')
-      })
-    )
-
-    // The webextension-polyill doesn't work well with webpacks ProvidePlugin.
-    // So we need to monkey patch it on the fly
-    // More info: https://github.com/mozilla/webextension-polyfill/pull/86
-    config.module.rules.push({
-      test: /webextension-polyfill[\\/]+dist[\\/]+browser-polyfill\.js$/,
-      loader: require.resolve('string-replace-loader'),
-      options: {
-        search: 'typeof browser === "undefined"',
-        replace: 'typeof window.browser === "undefined" || Object.getPrototypeOf(window.browser) !== Object.prototype'
-      }
-    })
-  }
-
   // Set environment vars
   config.plugins.push(
     new webpack.EnvironmentPlugin({
